f40a0a8a1bfe0b3b37b9c32ca9c048ef449e2c96
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-15 01:44:09 +0430

    block almost finished

--- thanos.py
+++ thanos.py
@@ -1,5 +1,16 @@
 import hashlib
 
 class Block:
-    def __init__(self, nonce, hash, trx):
-        pass
+    def __init__(self, nonce, hash, previous_hash, trx):
+        self.nonce = nonce 
+        self.hash = hash 
+        self.trx = trx 
+        self.previous_hash = previous_hash
+    
+    def retrieve_information(self):
+        block_info = {
+            'previous_hash':self.previous_hash,
+            'hash': self.hash,
+            'nonce': self.nonce,
+            'trx': self.trx
+        }
