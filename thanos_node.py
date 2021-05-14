1465799695980194a59f91f4d8f7dac3c14cadd2
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-15 02:59:58 +0430

    A mining node is cool, isn't it?

---
+++ thanos_node.py
@@ -0,0 +1,47 @@
+from thanos import ThanosChain
+from flask import Flask, request 
+import uuid 
+
+node_id = uuid.uuid4()
+thanos_chain = ThanosChain()
+thanos_chain.create_genesis_block()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return {"message": "Welcome, my child"}
+
+@app.route('/create_transaction', methods=["POST"])
+def create_transaction():
+    data = request.json
+    sender = data['sender']
+    recipient = data['recipient']
+    amount = data['amount']
+    
+    thanos_chain.create_transaction(sender=sender, recipient=recipient, amount=amount)
+
+    return {"message" : "TRX updated"}
+
+@app.route('/transactions')
+def get_transactions():
+    return {"trx" : thanos_chain.temp_trx}
+
+@app.route('/blockchain')
+def get_chain():
+    return {"blockcain" : thanos_chain.chain}
+
+@app.route('/mine', methods=["POST"])
+def mine():
+    data = request.json
+    miner_device = data["device_id"]
+
+    reward_amount = 100
+    thanos_chain.create_transaction(sender=node_id, recipient=miner_device, amount=reward_amount)
+
+    thanos_chain.create_block()
+
+    return {"message" : "This is your destiny, my child"}
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6969)
