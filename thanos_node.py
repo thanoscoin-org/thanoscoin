2b8af82001c26749c7d1da2cd21718241c1ee89b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-15 15:39:15 +0430

    Making it fully decentralized

--- thanos_node.py
+++ thanos_node.py
@@ -1,13 +1,19 @@
 from thanos import ThanosChain
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
+import requests
 import sys
 import uuid 
 
 node_id = uuid.uuid4()
 node_id = str(node_id)
+port_number = sys.argv[1]
 thanos_chain = ThanosChain()
 thanos_chain.create_genesis_block()
 
+node_info = {"node_id": node_id, "address": "http://localhost:" + port_number}
+nodes = []
+nodes.append(node_info)
 app = Flask(__name__)
 
 @app.route('/')
@@ -45,7 +51,31 @@ def mine():
 
     return {"message" : "This is your destiny, my child"}
 
+@app.route("/nodes")
+def get_nodes():
+    return {"nodes" : nodes}
+
+@app.route("/nodes/single")
+def get_node():
+    return node_info
+
+@app.route("/nodes/append", methods=["POST"])
+def append_new_node():
+    node_to_append = request.json
+    print(node_to_append)
+    print(type(node_to_append))
+    nodes.append(node_to_append)
+
+    return {"added": node_to_append}
+
+@app.route("/nodes/append/request", methods=["POST"])
+def append_request():
+    headers = {'Content-type' : "application/json"}
+    data = json.dumps(node_info)
+    res = requests.post('http://localhost:8000/nodes/append', data=data, headers=headers)
+    print(res)
+    return {"status" : "success"}
+
 if __name__ == "__main__":
-    port_number = sys.argv[1]
     port_number = int(port_number)
     app.run(debug=True, port=port_number)
